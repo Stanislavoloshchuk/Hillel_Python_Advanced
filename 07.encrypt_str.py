429e208a6a7be96bcf2e7a10cfeedeb93856c906
StanislavVoloshchuk <voloshchukstanislav@gmail.com>
2020-02-28 23:32:53 +0200

    Update 07.encrypt_str.py

--- 07.encrypt_str.py
+++ 07.encrypt_str.py
@@ -11,3 +11,5 @@ input_str = 'aaaxxxxxxxxxxggggggggghhhhhhhssaaasssddee'
 group = ((c, len(list(g))) for c, g in itertools.groupby(input_str))
 encrypt_str = ''.join(c + (str(n) if n > 1 else '') for c, n in group)
 print(encrypt_str)
+
+# Output: a3x10g9h7s2a3s3d2e2
