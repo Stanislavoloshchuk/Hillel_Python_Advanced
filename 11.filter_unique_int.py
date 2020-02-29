dfdb4971d474da6e1289f7245c283e002835e4e6
StanislavVoloshchuk <voloshchukstanislav@gmail.com>
2020-02-29 12:16:24 +0200

    Update 11.filter_unique_int.py

--- 11.filter_unique_int.py
+++ 11.filter_unique_int.py
@@ -11,3 +11,5 @@ def filter_unique_int(input_list):
 
 input_list = filter_unique_int([10, 11, 2, 3, 5, 8, 23, 11, 2, 5, 76, 43, 2, 32, 76, 3, 10, 0, 1])
 print(input_list)
+
+# Output: [10, 11, 2, 3, 5, 8, 23, 76, 43, 32, 0, 1]
