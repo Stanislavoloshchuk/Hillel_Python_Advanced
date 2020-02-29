8332db7a4234f05e169a965cd4af218e60f4c72a
StanislavVoloshchuk <voloshchukstanislav@gmail.com>
2020-02-29 12:15:32 +0200

    Update 10.replace_vowels.py

--- 10.replace_vowels.py
+++ 10.replace_vowels.py
@@ -1,8 +1,9 @@
 """
 Заменить в произвольной строке согласные буквы на гласные.
 """
+
 try:
-    string = input("Input letters please: ")
+    string = input("Input text please: ")
     vowel = 'aeiouAEIOU'
     x = list(filter(lambda x: x not in vowel, string))
     x *= len(list(filter(lambda x: x in vowel, string))) // len(x) + 1
@@ -14,4 +15,8 @@ try:
         else:
             print(i, end='')
 except ZeroDivisionError:
-    print('Your string doesnt have a vowel')
+    print('Your string should contain at lease one consonant')
+    
+# Output: 
+# Input text please: The fool doth think he is wise, but the wise man knows himself to be a fool
+# ThT fh l dfth thlnk h  ds wtsh, b t tht whsn mkn kn ws hhms lf ts b  w fs,l
